2c4856c4ea17944952dc42a4e0109328bbfc951f
huanghorse <hanker.lv@foxmail.com>
2024-04-07 17:26:20 +0800

    add BinaryTree function

--- nonlinear_structure/BinaryTree.hpp
+++ nonlinear_structure/BinaryTree.hpp
@@ -4,16 +4,18 @@
 template<class T>
 struct BinaryTreeNode{
     T data;
-    BinaryTreeNode *left;
-    BinaryTreeNode *right;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
 
-    BinaryTreeNode (const T& data) : data(data) left(nullptr) right(nullptr) {}
+    BinaryTreeNode (const T& data) : data(data), left(nullptr), right(nullptr) {}
 };
 
 template<class T>
 class BinaryTree {
 private:
-    BinaryTreeNode *root;
+    BinaryTreeNode<T> *root;// 二叉树的根节点
+
+    BinaryTreeNode<T>* insertRecursive(BinaryTreeNode<T> *node, T value);
 public:
     BinaryTree() : root(nullptr) {}
     BinaryTree(const T& data) {
@@ -22,13 +24,49 @@ public:
     ~BinaryTree() {
         clear(root);
     }
-    bool createTree();
+
+    void insert(T value);
+    void remove(T value);
+    void modify(T value, int i);
+    int research(T value) const;
+
+    // 二叉树的前序遍历:访问根节点 -> 遍历左子树 -> 遍历右子树
+    void preOrderTraversal();
+    // 二叉树的中序遍历：遍历左子树 -> 访问根节点 -> 遍历右子树
+    void inOrderTraversal();
+    // 二叉树的后序遍历：遍历左子树 -> 遍历右子树 -> 访问根节点
+    void postOrderTraversal();
+
     void clear(BinaryTreeNode<T>* node);
 };
 
+template<class T>
+BinaryTreeNode<T>* BinaryTree<T>::insertRecursive(BinaryTreeNode<T>* node, T value) {
+    if (node == nullptr) {
+        return new BinaryTreeNode<T>(value);
+    }
+
+    if (value < node->data) {
+        node->left = insertRecursive(node->left, value);
+    } else if (value > node->data) {
+        node->right = insertRecursive(node->right, value);
+    }
+    return node; // 保持原样如果值已存在
+}
 
 template<class T>
-bool BinaryTree<T>::createTree() {}
+void BinaryTree<T>::insert(T value) {
+    root = insertRecursive(root, value);
+}
+
+template<class T>
+void BinaryTree<T>::remove(T value) {}
+
+template<class T>
+void BinaryTree<T>::modify(T value, int i) {}
+
+template<class T>
+int BinaryTree<T>::research(T value) const {}
 
 template<class T>
 void BinaryTree<T>::clear(BinaryTreeNode<T>* node) {
--- nonlinear_structure/test.cpp
+++ nonlinear_structure/test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "BinaryTree.hpp"
 
 int main(){
     return 0;
